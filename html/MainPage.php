118a624e4ec007bfbd79a28e580ab77cb5158eb2
caydendevelop <caydendevelop@gmail.com>
2020-12-05 16:24:05 +0800

    dimply the structure

--- html/MainPage.php
+++ html/MainPage.php
@@ -65,92 +65,7 @@
       //Display the Questions from qTable
       if(mysqli_num_rows($result) > 0)
       {
-        while($row = mysqli_fetch_array($result)) 
-        {
-          $qID = $row['qID'];
-          $qUp = json_decode($row['qUp']);
-          $qAnswer = json_decode($row['qAnswer']);
-          echo "<div class='card' data-id='".count($qUp)."' id='".$qID."'>
-                  <h4>".$row['qSpace']."</h4>
-                  <div class='leftSpan'>
-                    <span style='display:none'>qCreatorID: ".$row['qCreatorID']."</span>
-                    <h3>".$row['qCreatorName']."</h3>
-                    <h5>".$row['qTime']."</h5>
-                  </div>
-            
-                  <div class='rightSpan'>       
-                    <form action='QuestionDetailPage.php' method='POST'>  
-                      <input type='hidden' name='redirectQID' value=".$row['qID'].">
-                      <input type='submit' name='submit' value=".$row['qTitle'].">
-                    </form>
-                    <p>".$row['qContent']."</p>
-                  </div>
-            
-                  <div>
-                    <button id='upBtn+".$row['qID']."' name='".$row['qID']."' onclick='upvote(this)' >Upvote (".count($qUp).")</button>
-                    <button id='answerBtn+".$row['qID']."' name='".$row['qID']."' onclick='showAnswer(this)'>Answer (".count($qAnswer).")</button>
-                  </div>
-              " ;
-          $sql2 = "SELECT * FROM ansTable WHERE ansQID = '$qID' ORDER BY ansID DESC"; // Last entry First out :)
-          $result2 = mysqli_query($link, $sql2);  // $link from config.php , save the result to $result
-          //Display the Questions from qTable
-          if(mysqli_num_rows($result2) > 0)
-          {
-            echo"<div class='ansContainer noShow' id='ans_".$qID."' stat='off'>";
-
-            while($row2 = mysqli_fetch_array($result2)) 
-            {
-              echo"<div class='answerCard' >
-                    <span>".$row2['ansUserName']."</span>
-                    <span>posted on ".$row2['ansTime']."</span>
-                    <br/><br/>
-                    <div class='ansDiv'>
-                      ".$row2['ansContent']."
-                    </div>
-                  </div>";
-            }
-            if($_SESSION['user_logged_in'] === true) {
-              echo"<div class='answerCard'>
-                    <h3>$_SESSION[userName]</h3>
-                    <button onclick='showInput(this)' name='".$qID."'><h2>Post your new answer.</h2></button>
-                    <div id='ansDiv_".$qID."' class='ansDiv noShow' >
-                      <form action='./answerFunction.php' method='POST'>
-                        <input type='hidden' name='ansQID' value=".$row['qID'].">
-                        <textarea name='ansContent' id='ansContent_".$qID."' style='width:35em; height:8em;' required></textarea>
-                        <br/><br/>
-                        <input type='submit' name='ansButton' value='Submit' />
-                      </form>
-                      <button onclick='hideInput(this)' name='".$qID."'>Cancel</button>
-                    </div>
-                  </div>";
-            }
-            print "</div>";
-          } 
-          
-          else 
-          {
-            echo"<div class='ansContainer noShow' id='ans_".$qID."'>";
-
-            if($_SESSION['user_logged_in'] === true) {
-              echo"<div class='answerCard'>
-                    <h3>$_SESSION[userName]</h3>
-                    <button onclick='showInput(this)' name='".$qID."'><h2>Post your new answer.</h2></button>
-                    <div id='ansDiv_".$qID."' class='ansDiv noShow' >
-                      <form action='./answerFunction.php' method='POST'>
-                        <input type='hidden' name='ansQID' value=".$row['qID'].">
-                        <textarea name='ansContent' id='ansContent_".$qID."' style='width:35em; height:8em;' required></textarea>
-                        <br/><br/>
-                        <input type='submit' name='ansButton' value='Submit' />
-                      </form>
-                      <button onclick='hideInput(this)' name='".$qID."'>Cancel</button>
-                    </div>
-                  </div>";
-            }
-            print "</div>";
-          }
-
-          print "</div>";
-        }
+        include("renderCard.php");
       }
       print"</div>";
     ?>
@@ -219,24 +134,6 @@
           hi.appendChild(arr[i]); //将排好序的元素，重新塞到body里面显示。
       }
     }
-
-    // function hotFilter(){
-    //   let cardContainer = document.getElementById('cardContainer');
-    //   cardContainer.innerHTML = "";
-        
-    //   var xmlhttp = new XMLHttpRequest();
-          
-    //   xmlhttp.open("POST", "MainPageFunction.php", true);
-    //   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    //   xmlhttp.send("filter=Hot");
-
-    //   xmlhttp.onreadystatechange = function () {
-    //     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    //       var mesgs = document.getElementById("cardContainer");
-    //       mesgs.innerHTML = xmlhttp.responseText;
-    //     }
-    //   }
-    // }
 
     function upvote(para){
       
--- html/MainPageFunction.php
+++ html/MainPageFunction.php
@@ -2,253 +2,71 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 <?php
-  // ob_start();
-  
+
   //連接資料庫
   //只要此頁面上有用到連接MySQL就要include它
   include("config.php");
 
   if($_POST['navSearch'] != NULL) {
     $inputValue = $_POST['navSearch'];
-    // $_SESSION['Space'] = $inputValue;
+   
     //搜尋資料庫資料
     $sql = "SELECT * FROM qTable WHERE qTitle LIKE '%$inputValue%' ORDER BY qID DESC"; // Last entry First out :)
     $result = mysqli_query($link, $sql);  // $link from config.php , save the result to $result
 
     //Display the Questions from qTable
     if(mysqli_num_rows($result) > 0)
-    {
-      while($row = mysqli_fetch_array($result)) 
-        {
-          $qUp = json_decode($row['qUp']);
-          $qAnswer = json_decode($row['qAnswer']);
-          echo "<div class='card' data-id='".count($qUp)."'>
-                  <h4>".$row['qSpace']."</h4>
-                  <div class='leftSpan'>
-                    <span style='display:none'>qCreatorID: ".$row['qCreatorID']."</span>
-                    <h3>".$row['qCreatorName']."</h3>
-                    <h5>".$row['qTime']."</h5>
-                  </div>
-            
-                  <div class='rightSpan' id=".$row['qID'].">       
-                    <form action='QuestionDetailPage.php' method='POST'>  
-                      <input type='hidden' name='redirectQID' value=".$row['qID'].">
-                      <input type='submit' name='submit' value=".$row['qTitle'].">
-                    </form>
-                    <p>".$row['qContent']."</p>
-                  </div>
-            
-                  <div>
-                    <button id='upBtn+".$row['qID']."' name='".$row['qID']."' onclick='upvote(this)' '>Upvote (".count($qUp).")</button>
-                    <button id='answerBtn+".$row['qID']."' name='".$row['qID']."'>Answer (".count($qAnswer).")</button>
-                  </div>
-            
-                </div>";
-        }
-    }
+      {
+        include("renderCard.php");
+      }
   }
 
 
 
   if($_POST['filter'] =='Algorithm') {
     //搜尋資料庫資料
-    // $inputValue = $_POST['filter'];
-    // $_SESSION['Space'] = $inputValue;
     $sql = "SELECT * FROM qTable WHERE qSpace = 'Algorithm' ORDER BY qID DESC"; // Last entry First out :)
     $result = mysqli_query($link, $sql);  // $link from config.php , save the result to $result
 
     //Display the Questions from qTable
     if(mysqli_num_rows($result) > 0)
-    {
-      while($row = mysqli_fetch_array($result)) 
-        {
-          $qUp = json_decode($row['qUp']);
-          $qAnswer = json_decode($row['qAnswer']);
-          echo "<div class='card' data-id='".count($qUp)."'>
-                  <h4>".$row['qSpace']."</h4>
-                  <div class='leftSpan'>
-                    <span style='display:none'>qCreatorID: ".$row['qCreatorID']."</span>
-                    <h3>".$row['qCreatorName']."</h3>
-                    <h5>".$row['qTime']."</h5>
-                  </div>
-            
-                  <div class='rightSpan' id=".$row['qID'].">       
-                    <form action='QuestionDetailPage.php' method='POST'>  
-                      <input type='hidden' name='redirectQID' value=".$row['qID'].">
-                      <input type='submit' name='submit' value=".$row['qTitle'].">
-                    </form>
-                    <p>".$row['qContent']."</p>
-                  </div>
-            
-                  <div>
-                    <button id='upBtn+".$row['qID']."' name='".$row['qID']."' onclick='upvote(this)' '>Upvote (".count($qUp).")</button>
-                    <button id='answerBtn+".$row['qID']."' name='".$row['qID']."'>Answer (".count($qAnswer).")</button>
-                  </div>
-            
-                </div>";
-        }
-    }
+      {
+        include("renderCard.php");
+      }
   } elseif ($_POST['filter'] =='ML') 
   {
     //搜尋資料庫資料
-    // $inputValue = $_POST['filter'];
-    // $_SESSION['Space'] = $inputValue;
     $sql = "SELECT * FROM qTable WHERE qSpace = 'Machine Learning' ORDER BY qID DESC"; // Last entry First out :)
     $result = mysqli_query($link, $sql);  // $link from config.php , save the result to $result
 
     //Display the Questions from qTable
     if(mysqli_num_rows($result) > 0)
     {
-      while($row = mysqli_fetch_array($result)) 
-        {
-          $qUp = json_decode($row['qUp']);
-          $qAnswer = json_decode($row['qAnswer']);
-          echo "<div class='card' data-id='".count($qUp)."'>
-                  <h4>".$row['qSpace']."</h4>
-                  <div class='leftSpan'>
-                    <span style='display:none'>qCreatorID: ".$row['qCreatorID']."</span>
-                    <h3>".$row['qCreatorName']."</h3>
-                    <h5>".$row['qTime']."</h5>
-                  </div>
-            
-                  <div class='rightSpan' id=".$row['qID'].">       
-                    <form action='QuestionDetailPage.php' method='POST'>  
-                      <input type='hidden' name='redirectQID' value=".$row['qID'].">
-                      <input type='submit' name='submit' value=".$row['qTitle'].">
-                    </form>
-                    <p>".$row['qContent']."</p>
-                  </div>
-            
-                  <div>
-                    <button id='upBtn+".$row['qID']."' name='".$row['qID']."' onclick='upvote(this)' '>Upvote (".count($qUp).")</button>
-                    <button id='answerBtn+".$row['qID']."' name='".$row['qID']."' >Answer (".count($qAnswer).")</button>
-                  </div>
-            
-                </div>";
-        }
+      include("renderCard.php");
     }
   } elseif ($_POST['filter'] =='System') 
   {
     //搜尋資料庫資料
-    // $inputValue = $_POST['filter'];
-    // $_SESSION['Space'] = $inputValue;
     $sql = "SELECT * FROM qTable WHERE qSpace = 'System' ORDER BY qID DESC"; // Last entry First out :)
     $result = mysqli_query($link, $sql);  // $link from config.php , save the result to $result
 
     //Display the Questions from qTable
     if(mysqli_num_rows($result) > 0)
     {
-      while($row = mysqli_fetch_array($result)) 
-        {
-          $qUp = json_decode($row['qUp']);
-          $qAnswer = json_decode($row['qAnswer']);
-          echo "<div class='card' data-id='".count($qUp)."'>
-                  <h4>".$row['qSpace']."</h4>
-                  <div class='leftSpan'>
-                    <span style='display:none'>qCreatorID: ".$row['qCreatorID']."</span>
-                    <h3>".$row['qCreatorName']."</h3>
-                    <h5>".$row['qTime']."</h5>
-                  </div>
-            
-                  <div class='rightSpan' id=".$row['qID'].">       
-                    <form action='QuestionDetailPage.php' method='POST'>  
-                      <input type='hidden' name='redirectQID' value=".$row['qID'].">
-                      <input type='submit' name='submit' value=".$row['qTitle'].">
-                    </form>
-                    <p>".$row['qContent']."</p>
-                  </div>
-            
-                  <div>
-                    <button id='upBtn+".$row['qID']."' name='".$row['qID']."' onclick='upvote(this)' '>Upvote (".count($qUp).")</button>
-                    <button id='answerBtn+".$row['qID']."' name='".$row['qID']."'>Answer (".count($qAnswer).")</button>
-                  </div>
-            
-                </div>";
-        }
+      include("renderCard.php");
     }
   } elseif ($_POST['filter'] =='Javascript') 
   {
     //搜尋資料庫資料
-    // $inputValue = $_POST['filter'];
-    // $_SESSION['Space'] = $inputValue;
     $sql = "SELECT * FROM qTable WHERE qSpace = 'Javascript' ORDER BY qID DESC"; // Last entry First out :)
     $result = mysqli_query($link, $sql);  // $link from config.php , save the result to $result
 
     //Display the Questions from qTable
     if(mysqli_num_rows($result) > 0)
     {
-      while($row = mysqli_fetch_array($result)) 
-        {
-          $qUp = json_decode($row['qUp']);
-          $qAnswer = json_decode($row['qAnswer']);
-          echo "<div class='card' data-id='".count($qUp)."'>
-                  <h4>".$row['qSpace']."</h4>
-                  <div class='leftSpan'>
-                    <span style='display:none'>qCreatorID: ".$row['qCreatorID']."</span>
-                    <h3>".$row['qCreatorName']."</h3>
-                    <h5>".$row['qTime']."</h5>
-                  </div>
-            
-                  <div class='rightSpan' id=".$row['qID'].">       
-                    <form action='QuestionDetailPage.php' method='POST'>  
-                      <input type='hidden' name='redirectQID' value=".$row['qID'].">
-                      <input type='submit' name='submit' value=".$row['qTitle'].">
-                    </form>
-                    <p>".$row['qContent']."</p>
-                  </div>
-            
-                  <div>
-                    <button id='upBtn+".$row['qID']."' name='".$row['qID']."' onclick='upvote(this)' '>Upvote (".count($qUp).")</button>
-                    <button id='answerBtn+".$row['qID']."' name='".$row['qID']."'>Answer (".count($qAnswer).")</button>
-                  </div>
-            
-                </div>";
-        }
+      include("renderCard.php");
     }
   }
 
-  // if($_POST['filter'] =='Hot') {
-  //   if( $_SESSION['Space'] != 'original') {
-  //     $cookie = $_SESSION['Space'];
-  //     $sql = "SELECT * FROM qTable WHERE qSpace = '$cookie' ORDER BY LENGTH(qUp) DESC"; // Last entry First out :)
-  //   }  
-  //   else{
-  //     $sql = "SELECT * FROM qTable ORDER BY LENGTH(qUp) DESC"; // Last entry First out :)
-  //   }
-  //   $result = mysqli_query($link, $sql);  // $link from config.php , save the result to $result
-
-  //   //Display the Questions from qTable
-  //   if(mysqli_num_rows($result) > 0)
-  //   {
-  //     while($row = mysqli_fetch_array($result)) 
-  //       {
-  //         $qUp = json_decode($row['qUp']);
-  //         echo "<div class='card'>
-  //                 <h4>".$row['qSpace']."</h4>
-  //                 <div class='leftSpan'>
-  //                   <span style='display:none'>qCreatorID: ".$row['qCreatorID']."</span>
-  //                   <h3>".$row['qCreatorName']."</h3>
-  //                   <h5>".$row['qTime']."</h5>
-  //                 </div>
-            
-  //                 <div class='rightSpan' id=".$row['qID'].">       
-  //                   <form action='QuestionDetailPage.php' method='POST'>  
-  //                     <input type='hidden' name='redirectQID' value=".$row['qID'].">
-  //                     <input type='submit' name='submit' value=".$row['qTitle'].">
-  //                   </form>
-  //                   <p>".$row['qContent']."</p>
-  //                 </div>
-            
-  //                 <div>
-  //                   <button id='upBtn+".$row['qID']."' name='".$row['qID']."' onclick='upvote(this)' '>Upvote (".count($qUp).")</button>
-  //                   <button>Answer</button>
-  //                 </div>
-            
-  //               </div>";
-  //       }
-  //   }
-  // } 
-  // ob_end_flush();
   
-
 ?>
